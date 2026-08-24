6146f999c7b7010141416e09b56e323531347121
DamianMartinDominguez <64601914+DamianMartinDominguez@users.noreply.github.com>
2026-08-24 16:23:09 -0300

    se cambio por las luces azules

--- PRUEBAesp8266.c
+++ PRUEBAesp8266.c
@@ -1,49 +1,60 @@
-#define altas 16
-#define bajas 5
-#define atras 4
-#define flujo1 0
-#define flujo2 2
-#define girod 14
-#define giroi 12
-#define reactor1 13
-#define reactor2 15
+// ======================================================
+// PRUEBA DE LUCES - DELOREAN
+// ESP8266 NodeMCU
+// ======================================================
+
+
+// ----------------------
+// PINES
+// ----------------------
+
+#define flujoab   16
+#define azules      5
+#define atras      4
+#define altas      0
+#define tableroa2  2
+#define girod     14
+#define flujoar   12
+#define interior  13
+#define giroi     15
+
 
 void setup()
 {
   // Debug console
-  Serial.begin(115200);
+  //Serial.begin(115200);
   delay(100);
   pinMode(atras,OUTPUT);
-  pinMode(bajas,OUTPUT);
+  pinMode(azules,OUTPUT);
   pinMode(altas,OUTPUT);
-  pinMode(flujo1,OUTPUT);
-  pinMode(flujo2,OUTPUT);
+  pinMode(tableroa2,OUTPUT);
   pinMode(girod,OUTPUT);
+  pinMode(flujoar,OUTPUT);
+  pinMode(interior,OUTPUT);
   pinMode(giroi,OUTPUT);
-  pinMode(reactor1,OUTPUT);
-  pinMode(reactor2,OUTPUT);
+  //pinMode(reactor2,OUTPUT);
 
 }
 
 void loop() {
-  digitalWrite(atras, HIGH);
-  digitalWrite(bajas, HIGH);
-  digitalWrite(altas, HIGH);
-  digitalWrite(flujo1, HIGH);
-  digitalWrite(flujo2, HIGH);
-  digitalWrite(giroi, HIGH);
-  digitalWrite(girod, HIGH);
-  digitalWrite(reactor1, HIGH);
-  digitalWrite(reactor2, HIGH);
-   delay(500);
+  digitalWrite(atras, LOW);//luces marcha atras
+  digitalWrite(azules, LOW);//luces marcha atras
+  digitalWrite(flujoab, LOW);//condensador de flujo bajas
+  digitalWrite(altas, LOW);//altas externas
+  digitalWrite(tableroa2, LOW);//luces amarillas del tablero trasero 
+  digitalWrite(flujoar, LOW);//condensador de flujo arriba
+  digitalWrite(girod, LOW);//giro derecho
+  digitalWrite(interior, LOW);//tablero y posicion(bajas)
+  digitalWrite(giroi, LOW);//giro izquierdo
+   delay(1500);
    digitalWrite(atras, HIGH);//luces marcha atras
-  digitalWrite(bajas, HIGH);//luces marcha atras
-  digitalWrite(altas, HIGH);//condensador de flujo bajas
-  digitalWrite(flujo1, HIGH);//altas externas
-  digitalWrite(flujo2, HIGH);//luces amarillas del tablero trasero 
-  digitalWrite(giroi, HIGH);//condensador de flujo arriba
-  digitalWrite(girod, HIGH);//giro derecho
-  digitalWrite(reactor1, LOW);//tablero y posicion(bajas)
-  digitalWrite(reactor2, HIGH);//giro izquierdo
-   delay(500);
+  digitalWrite(azules, HIGH);//luces marcha atras
+  digitalWrite(flujoab, LOW);//condensador de flujo bajas
+  digitalWrite(altas, LOW);//altas externas
+  digitalWrite(tableroa2, LOW);//luces amarillas del tablero trasero 
+  digitalWrite(flujoar, LOW);//condensador de flujo arriba
+  digitalWrite(girod, LOW);//giro derecho
+  digitalWrite(interior, LOW);//tablero y posicion(bajas)
+  digitalWrite(giroi, LOW);//giro izquierdo
+   delay(1500);
 }
